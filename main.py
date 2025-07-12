248661198a48ff275c8fd6382669844b8615af74
ShirFrangi <shir32004@gmail.com>
2025-07-12 21:04:55 +0300

    progress

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # -- Running Tests --
     test_all()
     initialize_database()
-    
+
     # -- Running Application --
     app = create_app()
     app.run(host='0.0.0.0', port=5001, debug=True)
